24255905e7bcb225e1e470afe66d0f3a11bc8de9
Ilya Zelenkov <ilya.zelenkov@torontomu.ca>
2025-01-09 15:30:13 -0600

    Update dataframe.cpp

--- dataframe.cpp
+++ dataframe.cpp
@@ -7,14 +7,14 @@
 using namespace std;
 using ColumnData = variant<vector<int>, vector<float>, vector<string>>;
 
-// Version 1.0: 08/ 01 / 2025 
 // Implementing a Pandas style Dataframe.
 // Data stored as a hashmap data structure.
 // Ensures a O(1) complexity as attributes increase.
 // Less efficient at low attribute levels.
 
-// Version 1.1: 09 / 01 / 2025
+
 // Arithmatic operator have been overwritten to support vector math.
+
 
 // TODO: multi-type DataFrames.
 
@@ -47,9 +47,11 @@ public:
         {
             vector<int> temp;
 
-            for (int j = 0; j < data_in.size(); ++j)
+            temp.reserve(data_in.size());
+
+            for (const auto & j : data_in)
             {
-                temp.push_back(data_in.at(j).at(i));
+                temp.push_back(j.at(i));
             };
 
             attributeMap[attributes.at(i)] = temp;
@@ -63,9 +65,9 @@ public:
 
         cout << attribute << ": ";
 
-        for (int i = 0; i < v.size(); ++i)
+        for (int i : v)
         {
-            cout << v.at(i) << " ";
+            cout << i << " ";
         };
 
     };
@@ -106,7 +108,7 @@ public:
 };
 
 // Vector addition operator.
-vector<int> operator+(vector<int>& vec1, vector<int>& vec2)
+vector<int> operator+(const vector<int>& vec1, const vector<int>& vec2)
 {
     if (vec1.size() != vec2.size())
     {
@@ -124,7 +126,7 @@ vector<int> operator+(vector<int>& vec1, vector<int>& vec2)
 };
 
 // Vector subtraction operator.
-vector<int> operator-(vector<int>& vec1, vector<int>& vec2)
+vector<int> operator-(const vector<int>& vec1, const vector<int>& vec2)
 {
     if (vec1.size() != vec2.size())
     {
@@ -142,7 +144,7 @@ vector<int> operator-(vector<int>& vec1, vector<int>& vec2)
 };
 
 // Vector division operator.
-vector<float> operator/(vector<int>& vec1, vector<int>& vec2)
+vector<float> operator/(const vector<int>& vec1, const vector<int>& vec2)
 {
     if (vec1.size() != vec2.size())
     {
@@ -153,18 +155,18 @@ vector<float> operator/(vector<int>& vec1, vector<int>& vec2)
 
     for (int i = 0; i < vec1.size(); ++i)
     {
-        if (float(vec2.at(i)) == 0)
+        if (static_cast<float>(vec2.at(i)) == 0)
         {
             throw runtime_error("Value divisor at index: " + to_string(i) + " cannot be zero");
         }
-        temp[i] = float(vec1.at(i)) / float(vec2.at(i));
+        temp[i] = static_cast<float>(vec1.at(i)) / static_cast<float>(vec2.at(i));
     }
 
     return temp;
 };
 
 // Vector multiplication operator.
-vector<int> operator*(vector<int>& vec1, vector<int>& vec2)
+vector<int> operator*(const vector<int>& vec1, const vector<int>& vec2)
 {
     if (vec1.size() != vec2.size())
     {
